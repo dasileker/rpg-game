79052069066d1ee966e95747996e90e4aa5a34b7
zerradi <dasileker@gmail.com>
2021-04-01 17:37:21 +0100

    add option to the scene + add the laser for the player ship

--- src/scene/OptionsScene.js
+++ src/scene/OptionsScene.js
@@ -1,4 +1,6 @@
 import 'phaser';
+import Button from '../components/button';
+
 
 export default class OptionsScene extends Phaser.Scene {
   constructor () {
@@ -8,6 +10,46 @@ export default class OptionsScene extends Phaser.Scene {
   preload () {
   }
 
-  create () {
+  create() {
+    
+    this.audio = this.sys.game.globals.audio;
+
+    this.text = this.add.text(300, 100, 'Options', { fontSize: 40 });
+    this.musicButton = this.add.image(200, 200, 'checkedBox');
+    this.musicText = this.add.text(250, 190, 'Music Enabled', { fontSize: 24 });
+
+    this.soundButton = this.add.image(200, 300, 'checkedBox');
+    this.soundText = this.add.text(250, 290, 'Sound Enabled', { fontSize: 24 });
+
+    this.musicButton.setInteractive();
+    this.soundButton.setInteractive();
+
+    this.musicButton.on('pointerdown', () => {
+      this.audio.musicOn = !this.audio.musicOn;
+      this.updateAudio();
+    });
+
+    this.soundButton.on('pointerdown', () => {
+      this.audio.soundOn = !this.audio.soundOn;
+      this.updateAudio();
+    });
+
+    this.updateAudio();
+
+    this.menuButton = new Button(
+      this,
+      400,
+      500,
+      'blueButton1',
+      'blueButton2',
+      'Menu',
+      'Title',
+    );
+
+    this.updateAudio();
+  }
+
+  updateAudio() {
+    
   }
 };
