5333e269e03312ddb3c51b88e5456579c4588cd5
zerradi <dasileker@gmail.com>
2021-04-02 10:39:50 +0100

    add credits scene

--- src/scene/CreditsScene.js
+++ src/scene/CreditsScene.js
@@ -1,5 +1,7 @@
 import 'phaser';
 
+import config from '../config/config';
+
 export default class CreditsScene extends Phaser.Scene {
   constructor () {
     super('Credits');
@@ -8,6 +10,35 @@ export default class CreditsScene extends Phaser.Scene {
   preload () {
   }
 
-  create () {
+  create() {
+    
+    this.cerditsText = this.add.text(0, 0, 'Credits', {
+      fontSize: '32',
+      fill: '#fff',
+    });
+
+    this.zone = this.add.zone(
+      config.width / 2,
+      config.height / 2,
+      config.width,
+      config.height,
+    )
+
+    Phaser.Display.Align.In.Center(this.cerditsText, this.zone);
+
+    Phaser.Display.Align.In.Center(this.madeByText, this.zone);
+
+    this.madeByText.setY(900);
+
+    this.creditsTween = this.tweens.add({
+      target: this.cerditsText,
+      y: -100,
+      ease: 'Power1',
+      duration: 5000,
+      delay: 3000,
+    })
+
+
+
   }
 };
